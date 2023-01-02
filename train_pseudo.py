e3a2f0393f06430f7b6d3c314186eb3a52ea0ab8
ShigemichiMatsuzaki <matsuzaki@aisl.cs.tut.ac.jp>
2023-01-02 20:28:36 +0900

    [refactor] Remove unnecessary libraries

--- train_pseudo.py
+++ train_pseudo.py
@@ -3,34 +3,12 @@ __author__ = "ShigemichiMatsuzaki"
 __maintainer__ = "ShigemichiMatsuzaki"
 # ============================================
 
-import os
-import shutil
-import sys
-import traceback
-import datetime
-import collections
-from typing import Optional
 import random
 import numpy as np
 
 import torch
-from torch.utils.tensorboard import SummaryWriter
 from trainer.vanilla_trainer_with_optuna import PseudoTrainer
-from warmup_scheduler import GradualWarmupScheduler
-
 from options.train_options import TrainOptions
-from utils.metrics import AverageMeter, MIOU
-from utils.visualization import add_images_to_tensorboard, assign_label_on_features
-from utils.optim_opt import get_optimizer, get_scheduler
-from utils.model_io import import_model
-from utils.calc_prototype import calc_prototype
-
-from utils.dataset_utils import import_dataset, DATASET_LIST
-
-from loss_fns.segmentation_loss import UncertaintyWeightedSegmentationLoss, Entropy
-from utils.pseudo_label_generator import generate_pseudo_label
-
-from utils.logger import log_training_conditions, log_metrics
 
 
 def main():
