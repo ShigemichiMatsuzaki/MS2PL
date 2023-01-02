8313757ad9bcef88df754ee84f511f34fb14caba
ShigemichiMatsuzaki <matsuzaki@aisl.cs.tut.ac.jp>
2023-01-02 11:48:46 +0900

    [update] Fix parameters for soft pseudo-label

--- script/train_greenhouse_soft_pseudo.sh
+++ script/train_greenhouse_soft_pseudo.sh
@@ -1,31 +1,18 @@
 MODEL=espnetv2
 SOURCE_MODEL=espnetv2
-
+RESUME_FROM=./pretrained_weights/espnetv2_camvid_cityscapes_forest_best_iou_norm.pth
 # Parameters
 label_weight_temp=5 
 label_weight_threshold=0.1
-
-for use_kld_class_loss in true false; do
-for use_label_ent_weight in true false; do
-
-if [ ${use_kld_class_loss} = true ] && [ ${use_label_ent_weight} = true ]; then
-    continue
-fi
-if [ ${use_kld_class_loss} = false ] && [ ${use_label_ent_weight} = false ]; then
-    is_hard=true
-else
-    is_hard=false
-fi
-
-if [ ${use_kld_class_loss} = true ]; then
-    conf_thresh=0.80
-else
-    conf_thresh=0.95
-fi
+use_kld_class_loss=false
+use_label_ent_weight=true
+is_hard=false
+conf_thresh=0.95
 python train_pseudo.py \
     --device cuda \
     --model ${MODEL} \
     --use-cosine true \
+    --resume-from ${RESUME_FROM} \
     --target greenhouse \
     --batch-size 64 \
     --epoch 50 \
@@ -46,5 +33,3 @@ python train_pseudo.py \
     --sp-label-min-portion 0.9 \
     --pseudo-label-dir ./pseudo_labels/${SOURCE_MODEL}/ \
     --ignore-index 3 
-done
-done
